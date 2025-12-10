5c814885f0ccce6a6a65541e7607f28bcf10c8d5
Sakshi <sakshi.rani@arrise.com>
2025-12-10 13:39:00 +0530

    fix for user can create same poll if endtime is passed + exception handler

--- backend/src/main/java/com/sentinel/backend/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.sentinel.backend.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
