9f097468bc6bb4b9cd3acf52587919f6ca13338a
Sakshi <sakshi.rani@arrise.com>
2025-12-19 15:37:46 +0530

    Revert "test"
    This reverts commit 34b10cba540611954a6c37646b2a53c320677625.
    
    # Conflicts:
    #	backend/src/main/java/com/sentinel/backend/dto/response/ActivePollDTO.java
    #	backend/src/main/java/com/sentinel/backend/repository/ActivePollRepository.java
    #	backend/src/main/java/com/sentinel/backend/service/SignalServiceImpl.java

--- backend/src/main/java/com/sentinel/backend/entity/PollResultId.java
+++ backend/src/main/java/com/sentinel/backend/entity/PollResultId.java
@@ -2,16 +2,12 @@ package com.sentinel.backend.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Embeddable
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class PollResultId implements Serializable {
 
     @Column(name = "signal_id")
