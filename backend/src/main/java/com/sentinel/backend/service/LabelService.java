67e63122c907dc9cec3aacecf4c279d14fdbf378
amandeep.singh <amandeep.singh@arrise.com>
2026-01-12 18:42:27 +0530

    Revert "create label api"
    This reverts commit 7b466cb6f61b75e6b3b82862c4e8815e4f168b54.

--- backend/src/main/java/com/sentinel/backend/service/LabelService.java
+++
@@ -1,8 +0,0 @@
-package com.sentinel.backend.service;
-
-import com.sentinel.backend.dto.request.LabelCreateDTO;
-
-public interface LabelService {
-
-    void createLabel(LabelCreateDTO labelCreateDTO);
-}
