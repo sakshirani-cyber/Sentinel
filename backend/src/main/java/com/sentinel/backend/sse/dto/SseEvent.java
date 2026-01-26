34805b7adae61b4addab769b374ab47aabcf7358
Sakshi <sakshi.rani@arrise.com>
2026-01-26 14:05:49 +0530

    constructor

--- backend/src/main/java/com/sentinel/backend/sse/dto/SseEvent.java
+++ backend/src/main/java/com/sentinel/backend/sse/dto/SseEvent.java
@@ -2,10 +2,12 @@ package com.sentinel.backend.sse.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class SseEvent<T> {
 
